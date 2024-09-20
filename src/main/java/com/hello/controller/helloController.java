0f826edb2b3321ac965f78d085e1017e972eebf8
Sunsu <sunil.suseelan@oracle.com>
2024-09-20 09:46:22 +0530

    Testing app

--- src/main/java/com/hello/controller/helloController.java
+++ src/main/java/com/hello/controller/helloController.java
@@ -12,10 +12,14 @@ public class helloController {
 	public String index() {
 		return "index";
 	}
+
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
+
+
+
 
 }
