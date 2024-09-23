1b9322bb76999f9463d19a5b619534a08c4adba7
Sunsu <sunil.suseelan@oracle.com>
2024-09-23 13:30:14 +0530

    Adding logs

--- src/main/java/com/hello/controller/helloController.java
+++ src/main/java/com/hello/controller/helloController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Controller
 public class helloController {
 
@@ -15,10 +16,14 @@ public class helloController {
 
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		Logger logger = LoggerFactory.getLogger(this.getClass());
+		logger.info("Handling request for /greeting with parameter 'name': {}", name);
+
 		model.addAttribute("name", name);
+		logger.debug("Added attribute 'name' to model");
+
 		return "greeting";
 	}
-
 
 
 
