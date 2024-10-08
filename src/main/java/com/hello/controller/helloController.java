17ed05dc132c34933b170d069d0fd0483ab352e8
Sunsu <sunil.suseelan@oracle.com>
2024-10-08 08:27:41 +0530

    Undo logs

--- src/main/java/com/hello/controller/helloController.java
+++ src/main/java/com/hello/controller/helloController.java
@@ -8,23 +8,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 @Controller
 public class helloController {
-	private static final Logger logger = LoggerFactory.getLogger(helloController.class);
 
 	@RequestMapping("/hello")
 	public String index() {
-		// Add logging statement here
-		logger.info("Handling request for /hello");
-
 		return "index";
 	}
 
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		// Add logging statement here
-		logger.info("Handling request for /greeting with parameter 'name': {}", name);
 
 		model.addAttribute("name", name);
-		return "greeting";
+	return "greeting";
 	}
+
 
 }
