2217c290a9079c9b76cd042030fda01cafbdf37b
Sunsu <sunil.suseelan@oracle.com>
2024-10-09 17:02:21 +0530

    Adding logs to contoller class

--- src/main/java/com/hello/controller/helloController.java
+++ src/main/java/com/hello/controller/helloController.java
@@ -8,18 +8,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 @Controller
 public class helloController {
+	private static final Logger logger = LoggerFactory.getLogger(helloController.class);
 
 	@RequestMapping("/hello")
 	public String index() {
+		logger.info("Handling '/hello' request");
 		return "index";
 	}
 
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		logger.info("Handling '/greeting' request with name parameter: {}", name);
 
 		model.addAttribute("name", name);
 		return "greeting";
 	}
-
 
 }
